c2d1b37dd1876c0bfbf3b97751ac516f8aca8261
Lucas Weber <weberl48@outlook.com>
2015-08-28 14:20:34 -0600

    users can delete posts

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,11 +1,15 @@
 'use strict';
-app.controller('PostsCtrl', function($scope){
-  $scope.posts= [];
-  $scope.post = {url: 'http://',title:""};
+app.controller('PostsCtrl', function ($scope) {
+    $scope.posts = [];
+    $scope.post = {url: 'http://', title: ''};
 
-  $scope.submitPost = function ($scope) {
-    $scope.posts.push($scope.post);
-    $scope.post = {url:'http://', title:''};
-  };
-});
+    $scope.submitPost = function () {
+      $scope.posts.push($scope.post);
+      $scope.post = {url: 'http://', title: ''};
+    };
+    $scope.deletePost = function (index) {
+      $scope.posts.splice(index, 1);
+};
+
+  });
 //controller for posts page
