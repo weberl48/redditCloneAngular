f62598f29c33c0476524689c753ba585ade0f4e4
Lucas Weber <weberl48@outlook.com>
2015-08-28 10:40:59 -0600

    app skeleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name redditCloneApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the redditCloneApp
+ */
+angular.module('redditCloneApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
